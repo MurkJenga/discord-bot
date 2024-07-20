7091a086f5844afdbefde86e320373b11385fd58
murkjenga <jeleonguwm@gmail.com>
2024-07-19 23:05:04 -0500

    Updating helper file

--- utils/helper.py
+++ utils/helper.py
@@ -9,7 +9,6 @@ def get_date_time(formatted=False):
 
 def change_tz(datetime, hours):
     return (datetime + timedelta(hours=hours)).strftime("%Y-%m-%d %H:%M:%S")
-    #2024-07-18 17:04:02
 
 def get_emoji_data(emoji):
             if isinstance(emoji, (discord.Emoji, discord.PartialEmoji)):
@@ -18,7 +17,6 @@ def get_emoji_data(emoji):
                     "id": str(emoji.id) if emoji.id else None
                 }
             else:
-                # Unicode emoji
                 return {
                     "name": str(emoji),
                     "id": None
